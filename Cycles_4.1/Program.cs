87b0e7fe8f202887cc4ff9ecbb1c99064c8712f5
Polchik <pyrina200052004@mail.ru>
2023-10-30 20:10:57 +1000

    Добавьте файлы проекта.

---
+++ Cycles_4.1/Program.cs
@@ -0,0 +1,52 @@
+﻿// задание 3
+//int.TryParse(Console.ReadLine(), out int chas);
+//int ameba = 1;
+//for(int i = 0; i<chas ; i+=3)
+//{
+//    ameba = ameba * 2;
+//}
+//Console.WriteLine(ameba);
+
+//задание 5
+//for (int i = 0; i <= 64; i += 4)
+//{
+//    Console.WriteLine("Кролики: " + i / 4 + "Гуси: " + (64 - i) / 2);
+//}
+
+//задача 6
+
+//for (int b = 0; b <= 10; b++)
+//{
+//    for (int k = 0; k <= 20; k++)
+//    {
+//        for (int t = 0; t <= 200; t++)
+//        {
+//            if ((b * 10 + k * 5 + t * 0.5) == 100 && (b + k + t == 100))
+//            {
+//                Console.WriteLine(b);
+//                Console.WriteLine(k);
+//                Console.WriteLine(t);
+
+//            }
+//        }
+//    }
+//}
+
+//задание 7
+//for (int a = 0; a < 2; a++)
+//{
+//    for (int b = 0; b < 2; b++)
+//    {
+//        for (int c = 0; c < 2; c++)
+//        {
+//            if ((a + b) == (b + a))
+//            {
+//                Console.WriteLine("true");
+//            }
+//            else { Console.WriteLine("false"); }
+
+
+//        }
+//    }
+//}
+
